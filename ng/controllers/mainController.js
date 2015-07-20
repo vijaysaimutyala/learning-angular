8fee1be316a10076baa3ce9754e650435ee8a23d
kneerunjun <kneerunjun@outlook.com>
2015-07-20 11:16:28 +0530

    + code for day01

---
+++ ng/controllers/mainController.js
@@ -0,0 +1,5 @@
+﻿(function () {
+    var mainController = angular.module("myApp").controller("mainController", function ($scope) {
+        $scope.name= "Niranjan Vijay Awati"
+    })
+})();
---
+++ ng/modules/myApp.js
@@ -0,0 +1,4 @@
+﻿(function () {
+    var myApp = angular.module("myApp", []);
+
+})();
