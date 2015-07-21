18078ef05d45f93dbf5f3e2eb4b256d8e31f7152
kneerunjun <kneerunjun@outlook.com>
2015-07-21 10:39:05 +0530

    + services injection

--- ng/controllers/mainController.js
+++ ng/controllers/mainController.js
@@ -1,28 +1,34 @@
 ﻿(function () {
-    var mainController = angular.module("myApp").controller("mainController", function ($scope, $log) {
-        var users = [
-            { email: "niranjan_awati", id: 41993, role: "manager" },
-            { email: "rahul_nair09", id: 1, role: "manager" },
-            { email: "pramod_prajapati", id:2 , role: "manager" },
-            { email: "ish_pandey", id: 4, role: "manager" },
-        ];
+    var mainController = angular.module("myApp").controller("mainController", function ($scope, $log, userService) {
+        
         $scope.loginInfo = { login: "", password: "" };
         $scope.authenticUser = null;
-
+        var users = [];
         $scope.onLogin = function () {
-            $log.debug("the login of the user is : " + $scope.loginInfo.login);
-            $log.debug("the password of the user is : " + $scope.loginInfo.password);
+            $log.debug("We are now waiting for the server to respond");
+            $log.debug("This is going to take 1500 milliseconds");
+            userService.getUsers().then(function (data) {
+                //this is when we have deferred.resolve();
+                $log.debug("we have received the data from the server");
+                users = data;
+                //$log.debug("the login of the user is : " + $scope.loginInfo.login);
+                //$log.debug("the password of the user is : " + $scope.loginInfo.password);
+                for (var i = 0; i < users.length; i++) {
+                    if (users[i].email == $scope.loginInfo.login && users[i].id == $scope.loginInfo.password) {
+                        $log.debug("the user is valid and with the role manager");
+                        $scope.authenticUser = true;
+                        break;
+                    }
+                    else {
+                        $scope.authenticUser = false;
+                    }
+                }
+            }, function (data) {
+                //this is run when we have deferred.reject()
+            });
+           
 
-            for (var i = 0; i < users.length; i++) {
-                if (users[i].email == $scope.loginInfo.login && users[i].id == $scope.loginInfo.password) {
-                    $log.debug("the user is valid and with the role manager");
-                    $scope.authenticUser = true;
-                    break;
-                }
-                else {
-                    $scope.authenticUser = false;
-                }
-            }
+           
             
         }
 
---
+++ ng/services/userService.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    //iffe - immediately invoked function expression
+    var userService = angular.module("myApp").service("userService", function ($q, $http, $timeout) {
+        return {
+            getUsers: function () {
+                var deferred = $q.defer();
+
+                var result =[
+                    { email: "niranjan_awati", id: 41993, role: "manager" },
+                    { email: "rahul_nair09", id: 1, role: "manager" },
+                    { email: "pramod_prajapati", id: 2, role: "manager" },
+                    { email: "ish_pandey", id: 4, role: "manager" },
+                ];
+
+                //is on another thread
+                $timeout(function () {
+                    deferred.resolve(result);
+                }, 2500)
+                //this is the main thread
+                return deferred.promise;
+
+            }
+        }
+    })
+})();
