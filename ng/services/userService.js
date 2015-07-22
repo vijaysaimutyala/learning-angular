a08d1c32e78d16756660e813291cd5a2abe03bd8
kneerunjun <kneerunjun@outlook.com>
2015-07-22 09:57:46 +0530

    + demo of the provider

--- ng/services/userService.js
+++ ng/services/userService.js
@@ -1,16 +1,12 @@
 ﻿(function () {
     //iffe - immediately invoked function expression
-    var userService = angular.module("myApp").service("userService", function ($q, $http, $timeout) {
+    var userService = angular.module("myApp").service("userService", function ($q,
+        $http, $timeout, valueProvider) {
         return {
             getUsers: function () {
                 var deferred = $q.defer();
 
-                var result =[
-                    { email: "niranjan_awati", id: 41993, role: "manager" },
-                    { email: "rahul_nair09", id: 1, role: "manager" },
-                    { email: "pramod_prajapati", id: 2, role: "manager" },
-                    { email: "ish_pandey", id: 4, role: "manager" },
-                ];
+                var result = valueProvider.userSet;
 
                 //is on another thread
                 $timeout(function () {
