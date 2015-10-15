d2445f799f39da72a8d2b51a42b0346fffd76ac7
kneerunjun <kneerunjun@outlook.com>
2015-10-15 10:08:54 +0530

    making a service and getting the generic logic

--- ng/controllers/loginController.js
+++ ng/controllers/loginController.js
@@ -1,27 +1,11 @@
 ﻿(function () {
-    var loginController = angular.module("myApp").controller("loginController", function ($scope, $http) {
+    var loginController = angular.module("myApp").controller("loginController", function ($scope, $http, loginService) {
         $scope.login = {
             username: "",
             password:""
         };
         $scope.attemptLogin = function () {
-            var url = "http://localhost:9090/webapi/login/authenticate"
-            //test comment
-            $http.post(url, $scope.login)
-                .then(function (response) {
-                    //this is the function that is executed when you have success from the service
-                    console.log("we have successful resposne from the service");
-                    console.log(response.data);
-                    console.log(response.status);
-            }, function (response) {
-                //this is the function when you have error from the service
-                console.log("we have unsuccessful resposne from the service");
-                console.log(response.status);
-                console.log(response.data.ExceptionMessage);
-            });
-            
-            console.log("hi there this is from inside of the controller");
-
+            loginService.login($scope.login);
         }
     })
 })(); 
---
+++ ng/services/loginService.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    var loginService = angular.module("myApp").service("loginService", function ($http, $q) {
+        this.baseUrl = "http://localhost:9090/webapi";
+        this.loginUrl = function () {
+            return this.baseUrl + "/login/authenticate"
+        }
+        this.loggedInUser = {};
+        this.login = function (login) {
+            $http.post(this.loginUrl(), login)
+               .then(function (response) {
+                   //this is the function that is executed when you have success from the service
+                   console.log("we have successful resposne from the service");
+                   console.log(response.data);
+                   console.log(response.status);
+               }, function (response) {
+                   //this is the function when you have error from the service
+                   console.log("we have unsuccessful resposne from the service");
+                   console.log(response.status);
+                   console.log(response.data.ExceptionMessage);
+               });
+            //this is the place where it return .. 
+        }
+    })
+})();
